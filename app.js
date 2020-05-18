457bfbcdd38312565f2917a60d26088e729931ed
Maria <maria.noaptes@student.tuiasi.ro>
2020-05-18 21:27:06 +0200

    functional login sistem with correct mongodb integration for appointments

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const fs = require('fs');
 const app = express();
 
 const port = 6789;
-
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static('public'))
@@ -20,8 +19,8 @@ app.use(cookieParser())
 app.use(session({ resave: false ,secret: '123456' , saveUninitialized: true, cookie: { secure: !true}}));
 app.get('/', (req, res) =>  res.render('index', {utilizator: req.session.utilizator}));
 app.get('/programari', (req, res) => {
+    console.log('acestea sunt'+req.session.programari)
     res.render('programari', {programari: req.session.programari, utilizator: req.session.utilizator});
-    console.log(req.session.programari)
 });
 app.get('/logare', (req, res) => {	
 	res.render('logare',{mesaj: req.cookies.mesaj, utilizator: req.session.utilizator});
@@ -30,17 +29,6 @@ app.get('/programare-noua', (req, res) => {
 	res.render('programare-noua',{utilizator: req.session.utilizator});
 });
 app.post('/programari', (req, res) => {
-    /*fs.readFile('utilizatori.json', (err, data) => {
-		if (err) throw err;
-        const listaUtilizatori = JSON.parse(data);
-        for(var i=0;i<Object.keys(listaUtilizatori.utilizatori).length;i++)
-        {
-            if(listaUtilizatori.utilizatori[i].username==req.session.utilizator)
-                listaUtilizatori.utilizatori[i].programari.push({"data":req.body.data, "stare":"in asteptare"})
-        }
-        let json = JSON.stringify(listaUtilizatori);
-                fs.writeFile('utilizatori.json', json);
-    })*/
     MongoClient.connect(url, function(err, client) {
         var db = client.db('db');
         var programari_client=db.collection('programari').findOne({username: req.session.utilizator})
@@ -59,48 +47,30 @@ app.post('/programari', (req, res) => {
     res.render('programari', {programari: req.session.programari, utilizator: req.session.utilizator});
     console.log(req.session.programari)
 });
-app.post('/verificare-logare', (req, res) => {	
-    var ok=0;
-    MongoClient.connect(url, function(err, client) {
-        var db = client.db('db');
-        if(db.collection('users').find({username: req.body.user, password: req.body.pass})!=null)
-        {
-            console.log("am gasit")
+async function retrive_progs(req,res)
+{   
+    await MongoClient.connect(url,async function(err, client) {
+            var db = client.db('db');
             res.cookie('mesaj','',{expires: new Date(1999,1,1)})
             req.session.utilizator=req.body.user;
-            var element=db.collection('programari').find({username: req.body.user})
-            req.session.programari=element.programari
-            ok=1;
-            client.close();
-            res.redirect('http://localhost:6789/')
-        }
-        if(ok==0)
-	{
-        console.log("am intrat si aici")
-		req.session.destroy((err) => {
-			if(err) {
-				return console.log(err);
-			}});
-		res.cookie('mesaj','Utilizator sau parola gresite', {expires: new Date(360000 + Date.now())})
-		res.redirect('http://localhost:6789/logare')
-	}
-    });
-	/*fs.readFile('utilizatori.json', (err, data) => {
-		if (err) throw err;
-		const listaUtilizatori = JSON.parse(data).utilizatori;
-		listaUtilizatori.forEach(element => {
-			if(req.body.user==element.username&&req.body.pass==element.parola)
-	{
-		res.cookie('mesaj','',{expires: new Date(1999,1,1)})
-        req.session.utilizator=req.body.user;
-        req.session.programari=element.programari
-		ok=1;
-		res.redirect('http://localhost:6789/')
-    }
-});*/
-	
-});
-	
-
-
+            await db.collection('programari').find({username:req.body.user}).toArray(function(err,docs){
+                if(docs[0]!=undefined){
+                    req.session.programari=docs[0]['programari'];
+                    res.redirect('http://localhost:6789/');   
+                }else
+                {
+                    req.session.destroy((err) => {
+                        if(err) {
+                            return console.log(err);
+                        }});
+                    res.cookie('mesaj','Utilizator sau parola gresite', {expires: new Date(360000 + Date.now())})
+                    res.redirect('http://localhost:6789/logare')
+                }
+                client.close();  
+    });     
+    })};
+app.post('/verificare-logare', (req, res) => {	
+    var ok=0;
+    retrive_progs(req,res);
+    })
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
--- create_database.js
+++ create_database.js
@@ -3,31 +3,10 @@ var url = 'mongodb://localhost';
 
 MongoClient.connect(url, function(err, client) {
   var db = client.db('db');
-  db.collection('users').insertOne({
-    username: "admin",
-    password: "admin"
-});
-  db.collection('users').insertOne({
-  username: "maria.noaptes",
-  password: "mnoaptes",
-  varsta:"21",
-  ocupatie:"student"
-});
-  db.collection('users').insertOne({
-  username: "alin34",
-  password: "alin34",
-  varsta:"36",
-  ocupatie:"youtuber"
-});
-  db.collection("programari").insertOne({
-    username: "maria.noaptes",
-    programari:[{"data":"16.02.2020", "stare": "efectuata"}]
-  })
-  db.collection("programari").insertOne({
-    username: "alin34",
-    programari:[{"data":"12.05.2020", "stare": "in asteptare"}]
-  })
-  console.log(db.collection('users').find());
-  console.log(db.collection('programari').find());
+  var doc = db.collection('programari').findOne({username:"alin34"});
+  console.log(doc)
+  if(doc!=null)
+      console.log(doc['programari']);
+
   client.close();
 }); 
